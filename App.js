6684fda4cf5e2988677b67bfdc359c451a09a456
JakeT285 <towjy006@mymail.unisa.edu.au>
2020-04-14 00:58:51 +0930

    Created basic UI for LoginScreen

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import LoginScreen from "./screens/loginScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <LoginScreen></LoginScreen>
     </View>
   );
 }
@@ -12,8 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: "grey",
+    // alignItems: "center",
+    // justifyContent: "center",
   },
 });
