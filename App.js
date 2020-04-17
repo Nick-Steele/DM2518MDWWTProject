6b690a685880e92f502953b4a122e1309a6caf0e
shuo <shuoc1218@gmail.com>
2020-04-17 15:48:36 +0200

    add list components and an simple example

--- App.js
+++ App.js
@@ -2,13 +2,18 @@ import React from "react";
 import { StyleSheet, View, } from "react-native";
 import LoginScreen from "./screens/loginScreen";
 import NewItem from "./screens/NewItem";
+import ShowList from "./components/Example";
+import { MenuProvider } from "react-native-popup-menu";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <LoginScreen></LoginScreen> */}
-      <NewItem></NewItem>
-    </View>
+    <MenuProvider>
+      <View style={styles.container}>
+        {/* <LoginScreen></LoginScreen> */}
+        {/* <NewItem></NewItem> */}
+        <ShowList></ShowList>
+      </View>
+    </MenuProvider>
   );
 }
 
