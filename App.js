b8b66dd7ad230f3e4bfa8a911a21826a680f0773
shuo <shuoc1218@gmail.com>
2020-04-22 13:31:05 +0200

    add,rm, edit, search methods

--- App.js
+++ App.js
@@ -3,14 +3,18 @@ import { StyleSheet, View } from "react-native";
 
 import MainNavigator from "./navigation/MainNavigator";
 import { LoginScreen, HomePage, NewItem } from "./screens";
+import Item from "./Helpers/Item";
+import TestChildScreen from "./screens/TestChildScreen";
 
 export default function App(props) {
   return (
     <View style={styles.container}>
-      {/*<LoginScreen/>*/}
-      {/*<NewItem></NewItem>*/}
-      <MainNavigator props={props} />
+      {/* <LoginScreen/> */}
+      {/* <NewItem></NewItem> */}
+      {/* <MainNavigator props={props} /> */}
       {/* <HomePage></HomePage> */}
+      <Item><TestChildScreen/></Item> 
+      {/* <TestChildScreen/> */}
     </View>
   );
 }
--- screens/TestChildScreen.js
+++ screens/TestChildScreen.js
@@ -1,10 +1,43 @@
-import * as React from 'react'
+import React,{Component} from 'react'
 import {View, Text, TouchableOpacity} from 'react-native'
 
-export default function TestChildScreen(){
+
+const userid = "epiNTkmnMtVwKKbFuPHP"
+const item1 = {"name":"potato","category":"vegetable", "amount":2.3,"location":"freeze", "expiredate":"2020-03-01"}
+const item2 = {"name":"tomato","category":"vegetable","amount":0.3,"location":"freeze", "expiredate":"2020-03-02"}
+const item3 = {"name":"carrot","category":"vegetable","amount":0.3,"location":"freeze", "expiredate":"2020-03-03"}
+const editid = "IT9iLcaw7PUV4UP3cyZY"
+
+class TestChildScreen extends Component {
+	
+	constructor(props){
+		super()
+		this.state={
+			"fooddisplay":[],
+		}
+	}
+
+	render(){
+	// test function
+	// var foodlist = this.props.getItemFromItems(userid)
+	// foodlist.then(x=>console.log("current food list:",x))
+	// var searchitem = this.props.searchItemFromItems("potato")
+	// searchitem.then(x=>console.log("search result:",x))
+	var newaddlist = this.props.addItemToItems(userid,item3)
+	newaddlist.then(x=>console.log("new add list:",x))
+	// var newaddlist = this.props.addItemToItems(userid,item3)
+	// newaddlist.then(x=>console.log("new add list:",x))
+	// var newrmlist = this.props.removeItemFromItems(userid,editid)
+	// newrmlist.then(x=>console.log("new remove list:",x))
+	// var editlist = this.props.editItemInItems(userid,item3, editid)
+	// editlist.then(x=>console.log("new edit list:",x))
+
 	return (
 		<View>
 			<Text>TestChildScreen</Text>
 		</View>
 	)
 }
+}
+
+export default TestChildScreen
