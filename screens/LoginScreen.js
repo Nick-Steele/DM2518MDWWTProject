8c38865e59a3c9692c0b9b8c928bff45bc6ccb31
JakeT285 <towjy006@mymail.unisa.edu.au>
2020-04-24 13:23:41 +0930

    Fixed ui sizing issues with web version

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -8,53 +8,60 @@ import {
   TouchableOpacity,
   Alert,
   TextInput,
-  Platform
+  Platform,
 } from "react-native";
-import * as Google from 'expo-google-app-auth';
-import {IOS_CLIENT_ID, ANDROID_CLIENT_ID} from 'react-native-dotenv';
+import * as Google from "expo-google-app-auth";
+import { IOS_CLIENT_ID, ANDROID_CLIENT_ID } from "react-native-dotenv";
 import Firebase from "../config/Firebase";
 import firebase from "firebase";
 
 const onSignIn = (googleUser) => {
   // We need to register an Observer on Firebase Auth to make sure auth is initialized.
-  var unsubscribe = firebase.auth().onAuthStateChanged(function(firebaseUser) {
+  var unsubscribe = firebase.auth().onAuthStateChanged(function (firebaseUser) {
     unsubscribe();
     // Check if we are already signed-in Firebase with the correct user.
     if (!isUserEqual(googleUser, firebaseUser)) {
       // Build Firebase credential with the Google ID token.
       var credential = firebase.auth.GoogleAuthProvider.credential(
-          googleUser.idToken,
-          googleUser.accessToken);
+        googleUser.idToken,
+        googleUser.accessToken
+      );
       // Sign in with credential from the Google user.
-      firebase.auth().signInWithCredential(credential).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-      });
+      firebase
+        .auth()
+        .signInWithCredential(credential)
+        .catch(function (error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          // ...
+        });
     } else {
-      console.log('User already signed-in Firebase.');
+      console.log("User already signed-in Firebase.");
     }
   });
-}
+};
 
 const isUserEqual = (googleUser, firebaseUser) => {
   if (firebaseUser) {
     var providerData = firebaseUser.providerData;
     for (var i = 0; i < providerData.length; i++) {
-      if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
-          providerData[i].uid === googleUser.getBasicProfile().getId()) {
+      if (
+        providerData[i].providerId ===
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+        providerData[i].uid === googleUser.getBasicProfile().getId()
+      ) {
         // We don't need to reauth the Firebase connection.
         return true;
       }
     }
   }
   return false;
-}
+};
 
 const signIn = () => {
   var provider = new firebase.auth.GoogleAuthProvider();
@@ -66,10 +73,10 @@ async function signInWithGoogleAsync() {
     const result = await Google.logInAsync({
       androidClientId: ANDROID_CLIENT_ID,
       iosClientId: IOS_CLIENT_ID,
-      scopes: ['profile', 'email'],
+      scopes: ["profile", "email"],
     });
 
-    if (result.type === 'success') {
+    if (result.type === "success") {
       onSignIn(result);
       return result.accessToken;
     } else {
@@ -82,16 +89,19 @@ async function signInWithGoogleAsync() {
 
 const LoginScreen = () => {
   return (
-      <SafeAreaView style={styles.safeContainer}>
-        <View style={styles.container}>
-          <View style={styles.logoView}>
+    <SafeAreaView style={styles.safeContainer}>
+      <View style={styles.container}>
+        <View style={styles.logoContainer}>
+          <View style={styles.logoSizeView}>
             <Image
               style={styles.logo}
               source={require("../assets/mainLogo.png")}
             ></Image>
           </View>
+        </View>
 
-          <View style={styles.detailsContainer}>
+        <View style={styles.detailsContainer}>
+          <View style={styles.detailsSizeView}>
             <TextInput style={styles.userNameText} placeholder="Username..." />
             <TextInput
               style={styles.userNameText}
@@ -108,11 +118,13 @@ const LoginScreen = () => {
               </View>
             </TouchableOpacity>
           </View>
+        </View>
 
-          <View style={styles.btnContainer}>
+        <View style={styles.btnContainer}>
+          <View style={styles.btnSizeView}>
             <TouchableOpacity
               onPress={() => {
-                Platform.OS != 'web' ? signInWithGoogleAsync(): signIn();
+                Platform.OS != "web" ? signInWithGoogleAsync() : signIn();
               }}
             >
               <View style={styles.btnGoogleLoginView}>
@@ -157,7 +169,8 @@ const LoginScreen = () => {
             </TouchableOpacity>
           </View>
         </View>
-      </SafeAreaView>
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -168,24 +181,38 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  logoView: {
+
+  logoContainer: {
     flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logoSizeView: {
+    flex: 1,
+    maxWidth: 500,
+    maxHeight: 500,
+    alignSelf: "stretch",
     alignItems: "center",
     justifyContent: "center",
-    // backgroundColor: "grey",
   },
   logo: {
-    width: 280,
-    height: 280,
-    // borderRadius: 150,
-    alignItems: "center",
+    flex: 1,
+    resizeMode: "contain",
+    minWidth: 300,
   },
+
   detailsContainer: {
     flex: 1,
     paddingLeft: 60,
     paddingRight: 60,
+    flexDirection: "row",
     justifyContent: "center",
-    // backgroundColor: "lightgrey",
+    alignItems: "center",
+  },
+  detailsSizeView: {
+    flex: 1,
+    maxWidth: 600,
   },
   userNameText: {
     fontSize: 20,
@@ -201,19 +228,21 @@ const styles = StyleSheet.create({
     alignSelf: "flex-end",
     paddingTop: 8,
   },
+
   btnContainer: {
     flex: 1,
-    justifyContent: "center",
     paddingLeft: 50,
     paddingRight: 50,
-    // backgroundColor: "skyblue",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
   },
   btnGoogleLoginView: {
     borderRadius: 5,
     marginBottom: 24,
+    flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
-    flexDirection: "row",
     backgroundColor: "#db4a37",
   },
   googleImage: {
@@ -252,6 +281,10 @@ const styles = StyleSheet.create({
   },
   btnTermsText: {
     color: "grey",
+  },
+  btnSizeView: {
+    flex: 1,
+    maxWidth: 600,
   },
 });
 
