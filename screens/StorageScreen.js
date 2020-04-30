b48c9d2345268a0755c6a3c3aee9a3a7a4ab14a7
Nick <nicksteele011@gmail.com>
2020-04-30 21:51:54 +0930

    Added static example data to storage screen.
    Example completes the prototype of the application.
    Each page has some form of implementation.

--- screens/StorageScreen.js
+++ screens/StorageScreen.js
@@ -1,19 +1,66 @@
 import * as React from "react";
-import { View, Text, TouchableHighlight } from "react-native";
+import { View, Text, TouchableHighlight, FlatList, Button } from "react-native";
 import Item from "../Helpers/Item";
+import Listitem from "../components/Listitem";
+import { MenuContext } from "react-native-popup-menu";
 
 export default function StorageScreen({ navigation }) {
   //   const item = Item();
+  const TestData = {
+    food: [
+      {
+        title: "Cabbage",
+        category: "vegetable",
+        location: "Pantry",
+        amount: 1,
+        date: "2020-04-17",
+      },
+      {
+        title: "Cabbage",
+        category: "vegetable",
+        location: "Pantry",
+        amount: 2,
+        date: "2020-04-17",
+      },
+      {
+        title: "Cabbage",
+        category: "vegetable",
+        location: "Pantry",
+        amount: 3,
+        date: "2020-04-17",
+      },
+      {
+        title: "Cabbage",
+        category: "vegetable",
+        location: "Pantry",
+        amount: 4,
+        date: "2020-04-17",
+      },
+    ],
+  };
   return (
     <View>
-      <Text>StorageScreen</Text>
-      <TouchableHighlight
-        onPress={() => {
-          navigation.push("TestChildScreen");
-        }}
-      >
-        <Text style={{ color: "#007AFF" }}>Yai</Text>
-      </TouchableHighlight>
+      <View>
+        <Text>StorageScreen</Text>
+        <Button
+          title="Test Child Screen"
+          onPress={() => {
+            navigation.push("TestChildScreen");
+          }}
+        ></Button>
+      </View>
+      <MenuContext>
+        <Item>
+          <View style={{ marginTop: 30 }}>
+            <FlatList
+              data={TestData.food}
+              renderItem={({ item }) => {
+                return Listitem(item);
+              }}
+            />
+          </View>
+        </Item>
+      </MenuContext>
     </View>
   );
 }
